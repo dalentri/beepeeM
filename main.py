66b47910a4dd1685ee3f8777c6860d8070fea978
dalentri <aydenjmason05@gmail.com>
2026-03-12 22:02:02 -0500

    Moved code to a class, moved listener init to a method, and added a bpm calculation

--- main.py
+++ main.py
@@ -3,27 +3,61 @@ from time import perf_counter
 from collections import deque
 
 
-dq = deque([])
+class BPM_machine:
+    def __init__(self, size=8) -> None:
+        self.last_tap_time = None
+        self.current_tap_time = None
+        self.bpm = None
 
-current_tap_time = None
-last_tap_time = None
+        # Inits a deque that has a max len of the defined size
+        self.buffer = deque(maxlen=size)
 
+    def calculate_bpm(self, buffer):
+        if len(self.buffer) > 0:
+            # Take the sum of the dequeue buffer and divide by the total num of elements
+            avg_interval = sum(self.buffer) / len(self.buffer)
+            self.bpm = 60 / avg_interval
 
-# Key handling
-def on_press(key):
-    if key == keyboard.Key.space:
-        tap_start = perf_counter()
+    # Key handling
+    def on_press(self, key):
 
-    if key == keyboard.Key.esc:
-        return False
+        print("tap")
 
+        # bpm tap button
+        if key == keyboard.Key.space:
+            # If its the first tap
+            if self.last_tap_time is None and self.current_tap_time is None:
+                self.last_tap_time = perf_counter()
 
-# Init listener that watches for key presses
-listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+            # If there was a prior tap
+            elif self.last_tap_time is not None:
+                # Get the current tap time
+                self.current_tap_time = perf_counter()
+                # calc the difference
+                delta = self.current_tap_time - self.last_tap_time
+                # Add the delta to the dequeue
+                self.buffer.append(delta)
+                # Make the current tap the last tap for the next run
+                self.last_tap_time = self.current_tap_time
+
+                self.calculate_bpm(self.buffer)
+                print(f"Current BPM: {self.bpm}")
+
+        # Close the listener
+        if key == keyboard.Key.esc:
+            return False
+
+    def run(self):
+        # Init listener that watches for key presses
+        with keyboard.Listener(on_press=self.on_press) as listener:
+            listener.join()
 
 
 def main():
-    print("Tap the space key along to the music :>")
+    bpm_finder = BPM_machine()
+
+    print("Tap the space key along to the music (esc to quit) :>")
+    bpm_finder.run()
 
 
 if __name__ == "__main__":
